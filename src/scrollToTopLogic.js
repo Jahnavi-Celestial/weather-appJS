532404740b783526c363a60f16c7924f738dc8bc
jahnavi-celestial <jahnavi.gaur@celestialsys.com>
2026-04-21 15:26:17 +0530

    eslint config

--- src/scrollToTopLogic.js
+++ src/scrollToTopLogic.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-vars */
 const scrollToTopLogic = (fDiv3) => {
     fDiv3.addEventListener('click',()=>{
         window.scrollTo({top: 0, behavior: 'smooth'});
-    })
-}
+    });
+};
